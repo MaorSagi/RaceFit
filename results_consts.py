9018949aff8cda68ce43b0a9efdb9ef3e5b128a6
MaorSagi <maors802@gmail.com>
2022-11-14 16:59:18 +0200

    Refactoring process finished - tested

--- results_consts.py
+++ results_consts.py
@@ -19,9 +19,11 @@ workout_src_dict = {"Training Peaks": "TP", "STRAVA": "STRAVA"}
 TEAM_NAMES = {1060.0: 'AG2R Citroën Team', 1103.0: 'CCC Team', 1330.0: 'Team Jumbo-Visma',
               2088.0: 'Lotto Fix All', 2738.0: 'Israel - Premier Tech', 1136.0: 'Cofidis', 1187.0: 'Groupama - FDJ',
               2040.0: 'Movistar Team', 1253.0: 'UAE Team Emirates', 1258.0: 'Trek - Segafredo'}
-TEAMS_RANKING_DICT = {'Team Jumbo-Visma': 1,'UAE Team Emirates': 4,'Groupama - FDJ': 8, 'Trek - Segafredo': 9,'AG2R Citroën Team': 12,
-                      'Cofidis': 15,'Movistar Team': 18, 'Lotto Fix All': 19,'Israel - Premier Tech': 20,'CCC Team': None}
-MISSING_WORKOUTS_TEAMS = {'UAE Team Emirates', 'Trek - Segafredo','Movistar Team'}
+TEAMS_RANKING_DICT = {'Team Jumbo-Visma': 1, 'UAE Team Emirates': 4, 'Groupama - FDJ': 8, 'Trek - Segafredo': 9,
+                      'AG2R Citroën Team': 12,
+                      'Cofidis': 15, 'Movistar Team': 18, 'Lotto Fix All': 19, 'Israel - Premier Tech': 20,
+                      'CCC Team': None}
+MISSING_WORKOUTS_TEAMS = {'UAE Team Emirates', 'Trek - Segafredo', 'Movistar Team'}
 
 EXTRA_LIM_GAP = 0.008
 
@@ -30,15 +32,15 @@ num_of_points = 10
 
 #    'cyclist_id',    'stage_id',    'race_id',
 strava_final_features_before_dropping_thresholds = [
-    'nation','continent', # todo: to remove
+    'nation', 'continent',  # todo: to remove
     'pcs_weight',
     'pcs_height',
     'age',
     'moving_time',
     'power_avg',
-    'speed_max',# todo: to remove
-    'total_time',# todo: to remove
-    'power_max',# todo: to remove
+    'speed_max',  # todo: to remove
+    'total_time',  # todo: to remove
+    'power_max',  # todo: to remove
     'training_load',
     'intensity',
     'weighted_norm_power',
@@ -49,7 +51,7 @@ strava_final_features_before_dropping_thresholds = [
     'elapsed_time',
     'calories',
     'speed_avg',
-    'cadence_max',# todo: to remove
+    'cadence_max',  # todo: to remove
     'energy',
     'number_of_workouts',
     'number_of_workouts_above_28_deg',
@@ -68,7 +70,7 @@ strava_final_features_before_dropping_thresholds = [
     'moving_time_last_year_avg_deviation',
     'energy_last_year_avg_deviation',
     'distance_last_year_avg_deviation',
-    'cadence_max_last_year_avg_deviation',# todo: to remove
+    'cadence_max_last_year_avg_deviation',  # todo: to remove
     'training_load_last_year_avg_deviation',
     'intensity_last_year_avg_deviation',
     'hr_avg_last_year_avg_deviation',
@@ -77,9 +79,9 @@ strava_final_features_before_dropping_thresholds = [
     'calories_last_year_avg_deviation',
     'elapsed_time_last_year_avg_deviation',
     'cadence_avg_last_year_avg_deviation',
-    'speed_max_last_year_avg_deviation',# todo: to remove
-    'total_time_last_year_avg_deviation',# todo: to remove
-    'power_max_last_year_avg_deviation',# todo: to remove
+    'speed_max_last_year_avg_deviation',  # todo: to remove
+    'total_time_last_year_avg_deviation',  # todo: to remove
+    'power_max_last_year_avg_deviation',  # todo: to remove
     'power_avg_last_year_avg_deviation',
     'weighted_norm_power_last_year_avg_deviation',
     'speed_avg_last_year_avg_deviation',
@@ -181,15 +183,15 @@ tp_final_features_before_dropping_thresholds = [
 ]
 
 strava_final_features_before_dropping_thresholds_formal = [
-    'cyclist last nation','cyclist last continent', # todo: to remove
+    'cyclist last nation', 'cyclist last continent',  # todo: to remove
     'cyclist weight',
     'cyclist height',
     'cyclist age',
     'workouts \ntotal moving time',
     'workouts \navg power',
-    'workouts \navg speed maxes', # todo: to remove
+    'workouts \navg speed maxes',  # todo: to remove
     'workouts \ntotal time',  # todo: to remove
-    'workouts \navg power maxes',# todo: to remove
+    'workouts \navg power maxes',  # todo: to remove
     'workouts \navg training load',
     'workouts \navg intensity',
     'workouts \navg norm power',
@@ -200,7 +202,7 @@ strava_final_features_before_dropping_thresholds_formal = [
     'workouts \nelapsed time',
     'workouts \ntotal calories',
     'workouts \navg speed',
-    'workouts \navg cadence maxes', # todo: to remove
+    'workouts \navg cadence maxes',  # todo: to remove
     'workouts \ntotal energy',
     'workouts count',
     'workouts count > 28 deg',
@@ -219,7 +221,7 @@ strava_final_features_before_dropping_thresholds_formal = [
     'workouts avg moving time \nlast year avg deviation',
     'workouts avg energy \nlast year avg deviation',
     'workouts avg distance \nlast year avg deviation',
-    'workouts avg cadence maxes \nlast year avg deviation', # todo: to remove
+    'workouts avg cadence maxes \nlast year avg deviation',  # todo: to remove
     'workouts avg training load \nlast year avg deviation',
     'workouts avg intensity \nlast year avg deviation',
     'workouts avg hr \nlast year avg deviation',
@@ -228,9 +230,9 @@ strava_final_features_before_dropping_thresholds_formal = [
     'workouts avg calories \nlast year avg deviation',
     'workouts avg elapsed time \nlast year avg deviation',
     'workouts avg cadence \nlast year avg deviation',
-    'workouts avg speed maxes \nlast year avg deviation',# todo: to remove
-    'workouts avg total time \nlast year avg deviation',# todo: to remove
-    'workouts avg power maxes \nlast year avg deviation',# todo: to remove
+    'workouts avg speed maxes \nlast year avg deviation',  # todo: to remove
+    'workouts avg total time \nlast year avg deviation',  # todo: to remove
+    'workouts avg power maxes \nlast year avg deviation',  # todo: to remove
     'workouts avg power \nlast year avg deviation',
     'workouts avg norm power \nlast year avg deviation',
     'workouts avg speed \nlast year avg deviation',
@@ -332,19 +334,21 @@ tp_final_features_before_dropping_thresholds_formal = [
 ]
 
 tp_features_names_dict = {k: v for k, v in
-                          zip(tp_final_features_before_dropping_thresholds, tp_final_features_before_dropping_thresholds_formal)}
+                          zip(tp_final_features_before_dropping_thresholds,
+                              tp_final_features_before_dropping_thresholds_formal)}
 strava_features_names_dict = {k: v for k, v in
-                          zip(strava_final_features_before_dropping_thresholds, strava_final_features_before_dropping_thresholds_formal)}
+                              zip(strava_final_features_before_dropping_thresholds,
+                                  strava_final_features_before_dropping_thresholds_formal)}
 
-BAR_PLOT_COLORS_DICT = {'race':'#1e90ff','stage':'#1e90ff','workouts':'#7eb54e','cyclist':'#f39C12'}
+BAR_PLOT_COLORS_DICT = {'race': '#1e90ff', 'stage': '#1e90ff', 'workouts': '#7eb54e', 'cyclist': '#f39C12'}
 
-ONE_DAY_RACES,MAJOR_TOURS, GRAND_TOURS = "One Day Races","Major Tours","Grand Tours"
+ONE_DAY_RACES, MAJOR_TOURS, GRAND_TOURS = "One Day Races", "Major Tours", "Grand Tours"
 
 curve_factor_x = 'recalls'  # 'precision_recall_curve'
 curve_factor_y = 'precisions'
-EXEC_NAME = "Oct 30 - rewrite exprs"#"Sep 6-rerun - J paper results"#
-EXEC_BASE_PATH = f"./executions/{EXEC_NAME}"#f"M:/Maor/Expr/maors_code/executions/{EXEC_NAME}"
-WORKOUTS_SRC = 'STRAVA' #'STRAVA'
+EXEC_NAME = "Oct 30 - rewrite exprs"  # "Sep 6-rerun - J paper results"#
+EXEC_BASE_PATH = f"./executions/{EXEC_NAME}"  # f"M:/Maor/Expr/maors_code/executions/{EXEC_NAME}"
+WORKOUTS_SRC = 'STRAVA'  # 'STRAVA'
 agg_func = 'SmartAgg'
 POPULARITY_IN_CONTINENT = "Popularity in continent"
 POPULARITY_IN_GENERAL = "Popularity in general"
@@ -359,16 +363,15 @@ features_names_dict = tp_features_names_dict if WORKOUTS_SRC == 'TP' else strava
 features_color_dict = {f: BAR_PLOT_COLORS_DICT[f.split()[0]] for f in features_names_dict.values()}
 
 SINGLE_RACE_TYPE = None
-SINGLE_TEAM = "Israel - Premier Tech" # "AG2R Citroën Team"  # "Israel - Premier Tech"
+SINGLE_TEAM = "Israel - Premier Tech"  # "AG2R Citroën Team"  # "Israel - Premier Tech"
 PLOT_AUC_PR = False
 
 AUC_PR_INTERACTION = False
 
-
 PLOT_EXPR_RESULTS = True
 with_baseline = True
 
-result_list =[] #[f"{EXEC_BASE_PATH}/['without', 5, 'STRAVA', 'Israel - Premier Tech']/[0.4, 'SimpleImputer']/['CatBoost']/ModelResults.csv"]  # [f"{EXEC_BASE_PATH}/[0.7, 0.7, 'SimpleImputer', 'without', 5, 'SmartAgg', 'TP', 'Israel - Premier Tech']/['CatBoost', None]/ModelResults.csv"]
+result_list = []  # [f"{EXEC_BASE_PATH}/['without', 5, 'STRAVA', 'Israel - Premier Tech']/[0.4, 'SimpleImputer']/['CatBoost']/ModelResults.csv"]  # [f"{EXEC_BASE_PATH}/[0.7, 0.7, 'SimpleImputer', 'without', 5, 'SmartAgg', 'TP', 'Israel - Premier Tech']/['CatBoost', None]/ModelResults.csv"]
 baseline_results_path = f"{EXEC_BASE_PATH}/Final_Baselines_Results.csv"
 model_results_path = f"{EXEC_BASE_PATH}/Final_Model_Results.csv"
 
@@ -379,7 +382,7 @@ PLOT_KN_RECALLS = False
 xpoints = range(K_POINTS)
 
 global_best_params_dict = {
-    'col_threshold': '0.4',#None,#'0.5',
+    'col_threshold': '0.4',  # None,#'0.5',
     'imputer': 'SimpleImputer',
     'imputer_workouts': 'without',
     # 'result_consideration':'[4,2,1]',
@@ -387,18 +390,17 @@ global_best_params_dict = {
     'model': 'CatBoost'
 }
 
-team_best_params_dict={'AG2R Citroën Team': {'col_threshold': '0.45'},
-                       'CCC Team': {'col_threshold': '0.45'},
-                       'Team Jumbo-Visma':{'col_threshold': '0.45'},
-                      'Lotto Fix All': {'col_threshold': '0.45'},
-                       'Israel - Premier Tech':{'col_threshold': '0.45'},
-                       'Cofidis': {'col_threshold': '0.45'},
-                       'Groupama - FDJ': {'col_threshold': '0.45'},
-                       'Movistar Team': {'col_threshold': '0.45'},
-                       'UAE Team Emirates': {'col_threshold': '0.45'},
-                       'Trek - Segafredo': {'col_threshold': '0.45'}
-                       }
-
+team_best_params_dict = {'AG2R Citroën Team': {'col_threshold': '0.45'},
+                         'CCC Team': {'col_threshold': '0.45'},
+                         'Team Jumbo-Visma': {'col_threshold': '0.45'},
+                         'Lotto Fix All': {'col_threshold': '0.45'},
+                         'Israel - Premier Tech': {'col_threshold': '0.45'},
+                         'Cofidis': {'col_threshold': '0.45'},
+                         'Groupama - FDJ': {'col_threshold': '0.45'},
+                         'Movistar Team': {'col_threshold': '0.45'},
+                         'UAE Team Emirates': {'col_threshold': '0.45'},
+                         'Trek - Segafredo': {'col_threshold': '0.45'}
+                         }
 
 PLOT_ONLY_BEST = False
 
@@ -412,10 +414,9 @@ params_to_plot = {'imputer': 'Imputer Type',
                   }
 imputation_labels = {'without': 'Without imputation', 'SimpleImputer': 'With imputation - SimpleImputer',
                      'KNNImputer': 'With imputation - KNNImputer'}  # {'without': 'Without imputation', 'SimpleImputer': 'With imputation'}
-model_labels = {'DecisionTree':'Decision Tree', 'CatBoost':'CatBoost', 'Logistic':'Logistic Regression'}
+model_labels = {'DecisionTree': 'Decision Tree', 'CatBoost': 'CatBoost', 'Logistic': 'Logistic Regression'}
 models_to_plot = ['DecisionTree', 'CatBoost', 'Logistic']
 # BEST_RUN_MODEL_DIR = f"['{best_params_dict['model']}', None]"
-
 
 
 PLOT_FEATURE_IMPORTANCE = False
@@ -423,14 +424,13 @@ FI_IN_TABLE = False
 FI_RANKING_TABLE = False
 
 SHAP, IG, RF, CATBST, CHI = True, True, True, True, True
-RFF= False
+RFF = False
 # BEST_MODEL_ITER = f"4_{best_params_dict['model']}.pkl"
 random_state = 0
 number_of_races_to_test_fi = 5
-interaction_feature = None#"distance_from_last_race"
+interaction_feature = None  # "distance_from_last_race"
 
-PLOT_CATBOOST_TREE=False
-
+PLOT_CATBOOST_TREE = False
 
 PLOT_TREE = False
 max_depth = 4
